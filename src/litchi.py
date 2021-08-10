47bda729d9c9fb6ec935ae01c48dd5b63760278f
onlycalm <741089410@qq.com>
2021-08-10 21:22:44 +0800

    feature(UI Win): 完善MainWin的UI设计。
    1、完善MainWin的UI设计；
    2、增加win.py文件；
    3、增加src目录结构，调整*.py文件到src目录下；

---
+++ src/litchi.py
@@ -0,0 +1,8 @@
+import win
+
+class cLitchi:
+    def __init__(self):
+        self.MainWin = win.cMainWin()
+
+    def show(self):
+        self.MainWin.show()
--- src/win.py
+++ src/win.py
@@ -1,12 +1,17 @@
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QFile, QIODevice
 
-class cLitchi:
+class cMainWin:
     def __init__(self):
-        MainWinUi = QFile("ui/MainWin.ui")         #要加载的ui文件
+        MainWinUi = QFile("ui/MainWin.ui")
         MainWinUi.open(QIODevice.ReadOnly)
-        self.MainWin = QUiLoader().load(MainWinUi) #加载UI设计
+        self.MainWin = QUiLoader().load(MainWinUi)
         MainWinUi.close()
+
+        self.MainWin.SndPb.clicked.connect(self.PtSnd)
 
     def show(self):
         self.MainWin.show()
+
+    def PtSnd(self):
+        print("PtSnd")
