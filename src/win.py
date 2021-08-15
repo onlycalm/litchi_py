a7e15910b41e1c0e2ef53eb0cd16df323b04a37a
onlyclam <741089410@qq.com>
2021-08-15 18:13:59 +0800

    feature(Btn): 增加按键响应函数。
    1、实现所有按键的点击事件响应；
    2、修改main.ui中控件的名称；
    3、修改gitignore文件配置；

--- src/win.py
+++ src/win.py
@@ -8,10 +8,28 @@ class cMainWin:
         self.MainWin = QUiLoader().load(MainWinUi)
         MainWinUi.close()
 
+        self.MainWin.OpnPtPb.clicked.connect(self.OpnPt)
+        self.MainWin.SvRecvPb.clicked.connect(self.SvRecv)
+        self.MainWin.ClrRecvPb.clicked.connect(self.ClrRecv)
+        self.MainWin.ClrSndPb.clicked.connect(self.ClrSnd)
         self.MainWin.SndPb.clicked.connect(self.PtSnd)
 
     def show(self):
         self.MainWin.show()
+
+    def OpnPt(self):
+        print("OpnPt")
+
+    def SvRecv(self):
+        print("SvRecv")
+
+    def ClrRecv(self):
+        print("ClrRecv")
+        self.MainWin.RecvTb.clear()
+
+    def ClrSnd(self):
+        print("ClrSnd")
+        self.MainWin.SndPtb.clear()
 
     def PtSnd(self):
         print("PtSnd")
