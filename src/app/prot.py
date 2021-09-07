f89cf797ef4d95061b29f161db6daff18362b3e8
onlycalm <741089410@qq.com>
2021-09-07 09:25:54 +0800

    feature(prot): 完善字符串协议帧的处理。
    1、晚上对字符串协议帧的格式提取和处理，先通过match函数检查是否符合预期格式；
    
    还不能处理异常数据，目前只通过正常数据的测试，待后续优化。

--- src/app/prot.py
+++ src/app/prot.py
@@ -8,17 +8,17 @@
 # @copyright Calm
 #
 
-import re
+from re import *
 
 ##
 # @class cStrFmtProt
 # @brief 字符串格式通讯协议类。
 # @details 该类用于解析字符串格式帧。
 # @note 帧格式为："<Str>: Num1, Num2, Num3...\n"
-#       1、<Str>为任意标识字符串；
-#       2、Numx为数据；
-#       3、\n为结束符；
-#       例：printf("chA: 1, 2, 3\nchB: 4, 5, 6\n")
+#   1.<Str>为任意标识字符串；
+#   2.Numx为数据；
+#   3.\n为结束符；
+#   例：printf("chA: 1, 2, 3\nchB: 4, 5, 6\n")
 # @attention 无
 #
 class cStrFmtProt:
@@ -39,30 +39,34 @@ class cStrFmtProt:
     # @param self 对象指针。
     # Wparam Msg 解析内容。
     # @return
-    #         - StrFrm 字符串帧解码结果。
-    #         - Msg 解析剩余内容。
-    # @note 无
+    #   - StrFrm 字符串帧解码结果。
+    #   - SurMsg 解析剩余内容。
+    # @note 帧格式为："<Str>: Num1, Num2, Num3...\n"
+    #   1.<Str>为任意标识字符串；
+    #   2.Numx为数据；
+    #   3.\n为结束符；
+    #   例：printf("chA: 1, 2, 3\nchB: 4, 5, 6\n")
     # @attention 无
     #
     def DecOnce(self, Msg):
         StrFrm = {}
+        SurMsg = Msg
 
         if len(Msg):
-            Srch = re.search(r"\n", Msg)
-            if Srch:
-                Idx = Srch.span()[0]
-                if Idx > 0:
-                    ExtStr = Msg[:Idx]
-                    Msg = Msg[Idx + 1:]
+            MtchRst = match(r"(\w+)(\s*):(\s*)(\d(\s*),(\s*))*(\d)(\s*)[\n\r|\r\n|\n]", Msg)
 
-                    if len(ExtStr):
-                        Id = re.sub(r":.*$", "", ExtStr)
-                        Dat = [int(x) for x in re.findall(r"\d+", ExtStr)]
+            if MtchRst:
+                Idx = MtchRst.span()[1]
+                SurMsg = Msg[Idx:]
+                Msg = Msg[:Idx]
+                Msg = sub(r"[\n\r|\r\n|\n].*$", "", Msg)
+                Id = sub(r":.*$", "", Msg)
+                Dat = [int(x) for x in findall(r"\d+", Msg)]
 
-                        if len(Id) and len(Dat):
-                            StrFrm = {"Id":Id, "Dat":Dat}
+                if len(Id) and len(Dat):
+                    StrFrm = {"Id":Id, "Dat":Dat}
 
-        return StrFrm, Msg
+        return StrFrm, SurMsg
 
     ##
     # @brief 字符串帧格式解析。
@@ -70,19 +74,24 @@ class cStrFmtProt:
     # @param self 对象指针。
     # Wparam Msg 解析内容。
     # @return
-    #         - StrFrm 字符串帧解码结果。
-    #         - Msg 解析剩余内容。
+    #   - StrFrm 字符串帧解码结果。
+    #   - SurMsg 解析剩余内容。
     # @note 无
     # @attention 无
     #
     def Dec(self, Msg):
         StrFrm = {}
+        SurMsg = ""
+
         DecRst, Msg = self.DecOnce(Msg)
         if DecRst:
             StrFrm[DecRst["Id"]] = DecRst["Dat"]
+            SurMsg = Msg
+
         while DecRst:
             DecRst, Msg = self.DecOnce(Msg)
             if DecRst:
                 StrFrm[DecRst["Id"]] = DecRst["Dat"]
+                SurMsg = Msg
 
-        return StrFrm, Msg
+        return StrFrm, SurMsg
